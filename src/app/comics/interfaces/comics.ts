fd4dd0fb97cb9dd71ca7b6564adae417c6ab0161
Néstor Espuch <nestorespuch@gmail.com>
2023-05-03 16:27:34 +0200

    Cambio logo

--- src/app/comics/interfaces/comics.ts
+++ src/app/comics/interfaces/comics.ts
@@ -1,28 +1,53 @@
-import { Auth } from "src/app/auth/interfaces/auth";
-
 export interface Comic {
-    id?: number;
+    id: number;
     title: string;
-    main_picture?:{medium:string,large:string};
-    // description: string;
-    // cuisine: string;
-    // daysOpen: string[];
-    // image: string;
-    // phone: string;
-    // creator?: Auth;
-    // mine?: boolean;
-    // distance?: number;
-    // commented?: boolean;
-    // stars?: number;
-    // address: string;
-    // lat: number;
-    // lng: number;
+    main_picture: {
+        medium: string;
+        large: string;
+    };
+    alternative_titles?: { en: string; ja: string };
+    start_date?: string;
+    end_date?: string;
+    synopsis?: string;
+    mean?: number;
+    rank?: number;
+    popularity?: number;
+    num_list_users?: number;
+    num_scoring_users?: number;
+    nsfw?: string;
+    genres?: [{id: number, name: string}];
+    created_at?: string;
+    updated_at?: string;
+    media_type?: string;
+    status?: string;
+    num_volumes?: number;
+    num_chapters?: number;
+    authors?: [node: { first_name: string; id: number; last_name: string }];
+    pictures?: [{medium: string, large: string}];
+    background?: string;
+    recommendations?: [
+        {
+            node: {
+                id: number;
+                main_picture: { medium: string; large: string };
+                title: string;
+            };
+            num_recommendations: number;
+        }
+    ];
+    serialization?: [node: { id: number; name: string }];
+    // my_list_status?: {
+    //     type: Object;
+    //     required: false;
+    // };
+    related_anime?: [string];
+    related_manga?: [string];
 }
-export interface Ranking{
-  rank:number;
+export interface Ranking {
+    rank: number;
 }
 
-export interface ComicyRanking{
-  node: Comic;
-  ranking: Ranking
+export interface ComicyRanking {
+    node: Comic;
+    ranking: Ranking;
 }
