ad42fcd59bba8287415ea4c76eaa8eae4cb6daaa
AndresMaurioDrapier <andresuqui2@gmail.com>
2023-05-07 17:51:33 +0200

    El filtro por genero sin llamada a la api por id

--- src/app/comics/pipes/comics-filter.pipe.ts
+++ src/app/comics/pipes/comics-filter.pipe.ts
@@ -8,31 +8,23 @@ import { Comic } from "../interfaces/comics";
     standalone: true,
 })
 export class ComicsFilterPipe implements PipeTransform {
-    service =inject(ComicsService);
+
     transform(comics: ComicyRanking[], genero: string): ComicyRanking[] {
         if (genero != "Filtrar" && genero) {
             const filteredComicRankings: ComicyRanking[] = [];
 
             for (const comicRanking of comics) {
 
-                this.getComics(comicRanking.node.id).subscribe(
-                    (comic: Comic) => {
-                      console.log(comic);
-                        if (
-                            comic.genres &&
-                            comic.genres.some((g) => g.name === genero)
-                        ) {
-                            filteredComicRankings.push(comicRanking);
-                        }
-                    }
-                );
+              if (
+                comicRanking.node.genres &&
+                comicRanking.node.genres.some((g) => g.name === genero)
+            ) {
+                filteredComicRankings.push(comicRanking);
+            }
             }
             return filteredComicRankings;
         } else {
             return comics;
         }
-    }
-    private getComics(id: number) {
-        return this.service.getIdComic(id);
     }
 }
