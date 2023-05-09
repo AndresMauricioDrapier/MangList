165c0bddfc4a4c87fb333e3333d7f9c53b84aa29
AndresMaurioDrapier <andresuqui2@gmail.com>
2023-05-09 18:52:40 +0200

    Subida de filtros en categorias

--- src/app/comics/pipes/comics-filter-category.pipe.ts
+++ src/app/comics/pipes/comics-filter-category.pipe.ts
@@ -11,15 +11,19 @@ export class ComicsFilterCategoryPipe implements PipeTransform {
 
     transform(comics: ComicyRanking[], genres?: string,status?:string,year?:string,order?:string): ComicyRanking[] {
 
-      const yearSplit = year?.split("-");
-      console.log(genres,status,order,year,yearSplit);
-
+      const yearSplit=year?.split("-");
+      if(yearSplit)
+      {
+        for(let i=+yearSplit[0], y=1;i < +yearSplit[1];i++,y++){
+          yearSplit.push(yearSplit[0]+1);
+        }
+      }
       //Filtrar por los comics segun lo que se reciba
       const comicsFiltrados = comics.filter((comicRanking: ComicyRanking) => {
         const comic: Comic = comicRanking.node;
         const ranking: Ranking | undefined = comicRanking.ranking;
         return (!genres || comic.genres?.some(genero=>genero.name===genres)) && (!status || comic.status === status) &&
-          (!year || comic.start_date?.includes(year)) && ranking;
+          (!year || comic.start_date?.includes(yearSplit?yearSplit[0]+"":"")) && ranking;
       });
 
       if(order)
