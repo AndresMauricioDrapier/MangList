44e45a4c909a08452ddce8a5fcef1e7b51008b86
Néstor Espuch <nestorespuch@gmail.com>
2023-05-18 20:15:47 +0200

    modal mobile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,18 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
-import { isMobile } from "./shared/validators/isMobile";
+import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { MenuComponent } from "./shared/menu/menu.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { CookiesComponent } from "./shared/cookies/cookies.component";
+import { MobileModalComponent } from "./shared/mobile-modal/mobile-modal.component";
 
 @Component({
     selector: "ml-root",
     standalone: true,
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
-    imports: [CommonModule, RouterModule, MenuComponent, FooterComponent, CookiesComponent]
+    imports: [CommonModule, RouterModule, MenuComponent, FooterComponent, CookiesComponent, MobileModalComponent]
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
     title = "MangList";
-    isMobile!: boolean;
-
-    ngOnInit(): void {
-        this.isMobile = isMobile();
-    }
-
-    closeModal(): void {
-        const modal = document.getElementById("modal")!;
-        modal.style.display = "none";
-    }
 }
